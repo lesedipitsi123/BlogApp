5789034fba3c08be78d3ec353b5ae017f9926247
lesedipitsi123 <lesedipitsi@gmail.com>
2022-09-25 20:45:22 +0200

    Removing empty body

--- app/src/main/java/com/devbytes/app/blogapp/ui/viewmodel/BlogViewModel.kt
+++ app/src/main/java/com/devbytes/app/blogapp/ui/viewmodel/BlogViewModel.kt
@@ -7,6 +7,4 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BlogViewModel
-@Inject constructor(private val repository: BlogRepository) : ViewModel(){
-
-}
+@Inject constructor(private val repository: BlogRepository) : ViewModel()
