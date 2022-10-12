59d1853d3ee6f5d362c7e90a6e42334454dbdec2
lesedipitsi123 <lesedipitsi@gmail.com>
2022-10-12 22:30:53 +0200

    Updating repos

---
+++ app/src/main/java/com/devbytes/app/blogapp/data/source/interfaces/AuthorRepository.kt
@@ -0,0 +1,15 @@
+package com.devbytes.app.blogapp.data.source.interfaces
+
+import com.devbytes.app.blogapp.data.model.Author
+import com.devbytes.app.blogapp.data.model.AuthorWithBlogs
+
+interface AuthorRepository {
+    suspend fun get(): List<Author>
+    suspend fun get(id: Int) : Author
+    suspend fun getAuthorsWithBlogs(): List<AuthorWithBlogs>
+    suspend fun getAuthorWithBlogs(authorId: Int): AuthorWithBlogs
+    suspend fun create(author: Author)
+    suspend fun update(author: Author)
+    suspend fun delete(author: Author)
+    suspend fun hasRecords(): Boolean
+}
