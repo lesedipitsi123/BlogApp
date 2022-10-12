57f010c688cb0c11f608697174060c79f04e7b0e
lesedipitsi123 <lesedipitsi@gmail.com>
2022-10-12 22:35:01 +0200

    Adding ui screens

--- app/src/main/java/com/devbytes/app/blogapp/ui/view/BlogScreen.kt
+++ app/src/main/java/com/devbytes/app/blogapp/ui/view/BlogScreen.kt
@@ -5,17 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.devbytes.app.blogapp.R
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.devbytes.app.blogapp.databinding.BlogScreenBinding
+import com.devbytes.app.blogapp.ui.view.adapters.BlogItemListAdapter
+import com.devbytes.app.blogapp.ui.viewmodel.BlogViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class BlogScreen : Fragment() {
+    private var _binding: BlogScreenBinding? = null
+    private val binding get() = _binding!!
+    private val blogViewModel by viewModels<BlogViewModel>()
+    private val blogAdapter: BlogItemListAdapter = BlogItemListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.blog_screen, container, false)
+    ): View {
+        _binding = BlogScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeComponents()
+    }
+
+    private fun initializeComponents() {
+        with(binding) {
+            blogViewModel.getBlogUiState()
+            lifecycleScope.launchWhenStarted {
+                blogViewModel.blogUiStateFlow.collect { uiState ->
+                    if (uiState.hasBlogs) {
+                        emptyBlog.root.visibility = View.GONE
+                        blogAdapter.submitList(uiState.blogItems)
+                        recyclerviewBlogs.apply {
+                            layoutManager = LinearLayoutManager(requireContext())
+                            adapter = blogAdapter
+                        }
+                    } else {
+                        emptyBlog.root.visibility = View.VISIBLE
+                    }
+                }
+            }
+        }
     }
 }
