bb47e9f545f02b0ac225c65c054124fb7da991e7
lesedipitsi123 <lesedipitsi@gmail.com>
2022-10-12 22:28:38 +0200

    Refactoring room entities

--- app/src/main/java/com/devbytes/app/blogapp/data/model/Author.kt
+++ app/src/main/java/com/devbytes/app/blogapp/data/model/Author.kt
@@ -1,17 +1,11 @@
 package com.devbytes.app.blogapp.data.model
 
 import androidx.room.Entity
-import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "author_table")
 data class Author(
-    var name: String,
-    @Ignore
-    var blogs: List<Blog>
-) {
     @PrimaryKey(autoGenerate = true)
-    var id: Int = 0
-
-    constructor() : this("", emptyList())
-}
+    val id: Int = 0,
+    val name: String,
+)
