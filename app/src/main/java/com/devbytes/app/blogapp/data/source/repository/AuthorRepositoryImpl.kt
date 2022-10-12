5e85ed8efec55f18c488036e2a70760db830fea5
lesedipitsi123 <lesedipitsi@gmail.com>
2022-10-12 22:31:11 +0200

    Updating repos

--- app/src/main/java/com/devbytes/app/blogapp/data/source/repository/AuthorRepositoryImpl.kt
+++ app/src/main/java/com/devbytes/app/blogapp/data/source/repository/AuthorRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.devbytes.app.blogapp.data.source.repository
 
 import com.devbytes.app.blogapp.data.model.Author
 import com.devbytes.app.blogapp.data.source.interfaces.AuthorDataSource
+import com.devbytes.app.blogapp.data.source.interfaces.AuthorRepository
 import com.devbytes.app.blogapp.dependency.annotation.LocalDataSource
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,11 +11,19 @@ import javax.inject.Singleton
 class AuthorRepositoryImpl @Inject constructor(
     @LocalDataSource private val dataSource: AuthorDataSource
 ) : AuthorRepository {
+    override suspend fun get() = dataSource.get()
+
     override suspend fun get(id: Int) = dataSource.get(id)
+
+    override suspend fun getAuthorsWithBlogs() = dataSource.getAuthorsWithBlogs()
+
+    override suspend fun getAuthorWithBlogs(authorId: Int) = dataSource.getAuthorWithBlogs(authorId)
 
     override suspend fun create(author: Author) = dataSource.create(author)
 
     override suspend fun update(author: Author) = dataSource.update(author)
 
     override suspend fun delete(author: Author) = dataSource.delete(author)
+
+    override suspend fun hasRecords() = dataSource.hasRecords()
 }
