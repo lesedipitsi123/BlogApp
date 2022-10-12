e6f5b42067e94c786a1a1a67f18121a3fa8b00d9
lesedipitsi123 <lesedipitsi@gmail.com>
2022-10-12 22:32:40 +0200

    Adding blog list adapter and view holder

---
+++ app/src/main/java/com/devbytes/app/blogapp/ui/view/adapters/BlogItemListAdapter.kt
@@ -0,0 +1,46 @@
+package com.devbytes.app.blogapp.ui.view.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.devbytes.app.blogapp.databinding.ItemListBlogBinding
+import com.devbytes.app.blogapp.ui.stateholder.BlogItemUiState
+
+
+class BlogItemListAdapter : ListAdapter<BlogItemUiState, BlogItemViewHolder>(BlogComparator) {
+    init {
+        hasStableIds()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlogItemViewHolder {
+        return BlogItemViewHolder(
+            ItemListBlogBinding.inflate(LayoutInflater.from(parent.context),parent, false )
+        )
+    }
+
+    override fun onBindViewHolder(holder: BlogItemViewHolder, position: Int) {
+        return holder.bind(getItem(position))
+    }
+
+    override fun getItemId(position: Int): Long {
+        return getItem(position).hashCode().toLong()
+    }
+    companion object {
+        private val BlogComparator = object : DiffUtil.ItemCallback<BlogItemUiState>(){
+            override fun areItemsTheSame(
+                oldItem: BlogItemUiState,
+                newItem: BlogItemUiState
+            ): Boolean {
+                return oldItem.title == newItem.title
+            }
+
+            override fun areContentsTheSame(
+                oldItem: BlogItemUiState,
+                newItem: BlogItemUiState
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
