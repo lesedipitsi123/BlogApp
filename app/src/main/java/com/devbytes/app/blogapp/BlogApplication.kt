c3df9f75733ef8b05217e779900f2f638710069d
lesedipitsi123 <lesedipitsi@gmail.com>
2022-11-26 10:35:04 +0200

    Adding dynamic colors

--- app/src/main/java/com/devbytes/app/blogapp/BlogApplication.kt
+++ app/src/main/java/com/devbytes/app/blogapp/BlogApplication.kt
@@ -1,7 +1,14 @@
 package com.devbytes.app.blogapp
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BlogApplication : Application()
+class BlogApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (DynamicColors.isDynamicColorAvailable())
+            DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
