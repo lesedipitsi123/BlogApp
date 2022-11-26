53150653d7b9b7b4c227c81f72b13381e6643a68
lesedipitsi123 <lesedipitsi@gmail.com>
2022-11-26 10:36:56 +0200

    Adding default value

--- app/src/main/java/com/devbytes/app/blogapp/data/model/Blog.kt
+++ app/src/main/java/com/devbytes/app/blogapp/data/model/Blog.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "blog_table")
 data class Blog(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val id: Int = 0,
     val authorId: Int,
     val title: String,
     val description: String
