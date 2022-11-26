5d6a8d17c49a2b27e8ab71454d3fb26354bb7bf2
lesedipitsi123 <lesedipitsi@gmail.com>
2022-11-26 10:26:18 +0200

    Adding new keys

--- app/src/main/java/com/devbytes/app/blogapp/util/Constants.kt
+++ app/src/main/java/com/devbytes/app/blogapp/util/Constants.kt
@@ -3,4 +3,7 @@ package com.devbytes.app.blogapp.util
 object Constants {
     const val DATABASE_NAME = "blog_app_data_base"
     const val DATABASE_VERSION = 1
+    const val KEY_AUTHOR_ID = "author_id"
+    const val KEY_AUTHOR_NAME = "author_name"
+    const val KEY_BLOG_ID = "blog_id"
 }
